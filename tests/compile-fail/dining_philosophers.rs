60d68fcd228ba8c49c13a0a7cd50211818c32394
SamGinzburg <ginzburg.sam@gmail.com>
2020-01-05 19:29:58 -0500

    added dining philosophers test example

---
+++ tests/compile-fail/dining_philosophers.rs
@@ -0,0 +1,65 @@
+#![feature(proc_macro_hygiene)]
+
+// error-pattern:panic
+
+extern crate threads_macro;
+
+use threads_macro::threads;
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+
+fn main() {
+	/*
+
+		5 person table
+
+		person 1: a, b
+		person 2: b, c
+		person 3: c, d
+		person 4: d, e
+		person 5: e, a
+
+	 */
+    threads!({locks = {a, b, c, d, e}}, {
+       loop {
+		   println!("think");
+		   a.lock().unwrap();
+		   b.lock().unwrap();
+		   println!("eat");
+		   sleep(1000);
+	   }
+	}, {
+		loop {
+			println!("think");
+			b.lock().unwrap();
+			c.lock().unwrap();
+			println!("eat");
+			sleep(1000);
+		}
+	}, {
+		loop {
+			println!("think");
+			c.lock().unwrap();
+			d.lock().unwrap();
+			println!("eat");
+			sleep(1000);
+		}
+	}, {
+		loop {
+			println!("think");
+			d.lock().unwrap();
+			e.lock().unwrap();
+			println!("eat");
+			sleep(1000);
+		}
+	}, {
+		loop {
+			println!("think");
+			e.lock().unwrap();
+			a.lock().unwrap();
+			println!("eat");
+			sleep(1000);
+		}
+	});
+}
